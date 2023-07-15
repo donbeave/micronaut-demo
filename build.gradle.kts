587752e9175b7a163a0aad59af5b6b74890bbfbd
Alexey Zhokhov <alexey@zhokhov.com>
2023-07-16 00:12:38 +0800

    Some config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,31 @@
 plugins {
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("io.micronaut.application") version "4.0.0"
+    id("io.micronaut.minimal.application") version "4.0.0"
     id("io.micronaut.aot") version "4.0.0"
+    id("io.micronaut.graalvm") version "4.0.0"
 }
 
 version = "0.1"
 group = "com.example"
 
 repositories {
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
-        mavenContent { snapshotsOnly() }
-    }
     mavenCentral()
 }
 
 dependencies {
-    annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
-    implementation("io.micronaut.serde:micronaut-serde-jackson")
-    compileOnly("org.graalvm.nativeimage:svm")
-    runtimeOnly("ch.qos.logback:logback-classic")
-    testImplementation("io.micronaut:micronaut-http-client")
+    runtimeOnly(mn.logback.classic)
 }
-
 
 application {
     mainClass.set("com.example.Application")
 }
+
 java {
-    sourceCompatibility = JavaVersion.toVersion("17")
-    targetCompatibility = JavaVersion.toVersion("17")
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(20)
+        vendor = JvmVendorSpec.ORACLE
+    }
 }
 
-graalvmNative.toolchainDetection.set(false)
 micronaut {
     runtime("netty")
     testRuntime("junit5")
@@ -39,21 +33,18 @@ micronaut {
         incremental(true)
         annotations("com.example.*")
     }
-    /*
     aot {
-        // Please review carefully the optimizations enabled below
-        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
-        optimizeServiceLoading.set(false)
-        convertYamlToJava.set(false)
+        optimizeServiceLoading.set(true)
+        convertYamlToJava.set(true)
         precomputeOperations.set(true)
         cacheEnvironment.set(true)
         optimizeClassLoading.set(true)
         deduceEnvironment.set(true)
         optimizeNetty.set(true)
     }
-    
-     */
 }
+
+graalvmNative.toolchainDetection.set(false)
 
 tasks {
     nativeCompile {
@@ -62,7 +53,7 @@ tasks {
             verbose.set(true)
             quickBuild.set(true) // for dev mode only, use GRAALVM_QUICK_BUILD instead
             buildArgs.add("-H:+ReportExceptionStackTraces")
-            useFatJar.set(true)
+            classpath.from.add(file("./build/libs/demo-0.1.jar"))
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+plugins {
+    id("io.micronaut.platform.catalog") version "4.0.0"
+}
 
-rootProject.name="demo"
-
+rootProject.name = "demo"
